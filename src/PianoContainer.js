2961aafb9a6e7685404152a07a5139cce6ce4b4e
Alex Sweeney <alexsweeney@live.co.uk>
2020-03-12 21:29:31 +0700

    feat(pianoContainer.js) added left function for keys

--- src/PianoContainer.js
+++ src/PianoContainer.js
@@ -14,22 +14,30 @@ class Piano extends Component {
 }
 
 class Octave extends Component { 
+	returnLeft(i, key) {
+		if(key == 'white') {
+			return (12.5 * i) + '%';
+		} else if (key == 'black') {
+			return (12.5 * i) + 9 + '%';
+		}
+	}
+
 	render() {
 		return (
 			<div className="octave">
 				<WhiteKey/>{/*C*/}
-					<BlackKey left={9+"%"}/>{/*C#*/}
-				<WhiteKey left={12.5+"%"}/>{/*D*/}
-					<BlackKey left={(12.5 + 9)+"%"}/>{/*D#*/}
-				<WhiteKey left={(12.5 * 2)+"%"}/>{/*E*/}
-				<WhiteKey left={(12.5 * 3)+"%"}/>{/*F*/}
-					<BlackKey left={(12.5 * 3)+ 9 +"%"}/>{/*F#*/}
-				<WhiteKey left={(12.5 * 4)+"%"}/>{/*G*/}
-					<BlackKey left={(12.5 * 4)+ 9 +"%"}/>{/*G#*/}
-				<WhiteKey left={(12.5 * 5)+"%"}/>{/*A*/}
-					<BlackKey left={(12.5 * 5) + 9 +"%"}/>{/*A#*/}
-				<WhiteKey left={(12.5 * 6)+"%"}/>{/*B*/}
-				<BlankKey left={(12.5 * 7)+"%"}/>{/*C*/}
+					<BlackKey left={this.returnLeft(0,'black')}/>{/*C#*/}
+				<WhiteKey left={this.returnLeft(1,'white')}/>{/*D*/}
+					<BlackKey left={this.returnLeft(1,'black')}/>{/*D#*/}
+				<WhiteKey left={this.returnLeft(2,'white')}/>{/*E*/}
+				<WhiteKey left={this.returnLeft(3,'white')}/>{/*F*/}
+					<BlackKey left={this.returnLeft(3,'black')}/>{/*F#*/}
+				<WhiteKey left={this.returnLeft(4,'white')}/>{/*G*/}
+					<BlackKey left={this.returnLeft(4,'black')}/>{/*G#*/}
+				<WhiteKey left={this.returnLeft(5,'white')}/>{/*A*/}
+					<BlackKey left={this.returnLeft(5,'black')}/>{/*A#*/}
+				<WhiteKey left={this.returnLeft(6,'white')}/>{/*B*/}
+				<BlankKey left={this.returnLeft(7,'white')}/>{/*C*/}
 			</div>
 		)
 	}
