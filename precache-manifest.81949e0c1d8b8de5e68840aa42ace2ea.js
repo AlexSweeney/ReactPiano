cc520a98564e5ee0f42edb6cd9d3cc01d2eaeb62
Alex Sweeney <alexsweeney@live.co.uk>
2021-06-28 18:04:27 +0700

    Updates

---
+++ precache-manifest.81949e0c1d8b8de5e68840aa42ace2ea.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d69b25eef5457d548c313a5a15ee79c",
+    "url": "/piano/index.html"
+  },
+  {
+    "revision": "34a560b45ddfa7f19b19",
+    "url": "/piano/static/css/main.eb335b20.chunk.css"
+  },
+  {
+    "revision": "623e65d645e23c39081d",
+    "url": "/piano/static/js/2.15db9d71.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/piano/static/js/2.15db9d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a560b45ddfa7f19b19",
+    "url": "/piano/static/js/main.1fc4ec51.chunk.js"
+  },
+  {
+    "revision": "db37ea1e0b40a386c9a2",
+    "url": "/piano/static/js/runtime-main.6fb3a419.js"
+  },
+  {
+    "revision": "157fdb4e86582c765fd7138a1c786e75",
+    "url": "/piano/static/media/A3.157fdb4e.mp3"
+  },
+  {
+    "revision": "3fd6228aecd6874073dabd60cef6fd3f",
+    "url": "/piano/static/media/Ab3.3fd6228a.mp3"
+  },
+  {
+    "revision": "ca302b66bbe20d7708e34188a9bdfd9e",
+    "url": "/piano/static/media/B3.ca302b66.mp3"
+  },
+  {
+    "revision": "a4556a712af618a4c68a4704afea1b76",
+    "url": "/piano/static/media/Bb3.a4556a71.mp3"
+  },
+  {
+    "revision": "58b0cf5ed989bc042a7d064a4f0ec846",
+    "url": "/piano/static/media/C3.58b0cf5e.mp3"
+  },
+  {
+    "revision": "2d262cf5274139cda321fcdb73df2cdd",
+    "url": "/piano/static/media/D3.2d262cf5.mp3"
+  },
+  {
+    "revision": "9e36e291bc183e787e9f6652a28672e5",
+    "url": "/piano/static/media/Db3.9e36e291.mp3"
+  },
+  {
+    "revision": "15ba0b838b093b6bd8dd507558917232",
+    "url": "/piano/static/media/E3.15ba0b83.mp3"
+  },
+  {
+    "revision": "8a189c5438c73f9111f9e3988ac052f6",
+    "url": "/piano/static/media/Eb3.8a189c54.mp3"
+  },
+  {
+    "revision": "0016c64e5d52c3f955dfd7446aa5107f",
+    "url": "/piano/static/media/F3.0016c64e.mp3"
+  },
+  {
+    "revision": "23448b11318eaea2fcae9c63ff7f7de7",
+    "url": "/piano/static/media/G3.23448b11.mp3"
+  },
+  {
+    "revision": "bfe6320f86ce14913b5e605c2a7ae73a",
+    "url": "/piano/static/media/Gb3.bfe6320f.mp3"
+  },
+  {
+    "revision": "02882ce29c39d46d8cb8c0eac28ce519",
+    "url": "/piano/static/media/correctSound.02882ce2.mp3"
+  },
+  {
+    "revision": "a41303f1a175d7dbabc402333b2a0364",
+    "url": "/piano/static/media/incorrectSound.a41303f1.mp3"
+  }
+]);
