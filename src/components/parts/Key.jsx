f64c5af542e04c8c6d9df102c8c241199b134ce9
Alex Sweeney <alexsweeney@live.co.uk>
2021-10-18 19:19:56 +0700

    feat(Key.jsx) find formula - must be 24 14 width ratio to work

--- src/components/parts/Key.jsx
+++ src/components/parts/Key.jsx
@@ -10,29 +10,38 @@ export default function Key({keyName, i, handleOver, handleOut, handleDown}) {
 
 	const realWhiteWidth = 24;
 	const realBlackWidth = 14;
+	
 
 	const whiteWidth = realWhiteWidth / 24;
 	const blackWidth = realBlackWidth / 24;
 
 	const keyColor = getKeyColor(keyName);
 	const thisWidth = (keyColor === 'white' ? whiteWidth : blackWidth) * size + 'px';
- 
-
+  
 	const style = {
 		left: (i * blackWidth) * size + 'px',
 		width: thisWidth,
 	}   
-	
+	 
+	// =========================== Find Formula ======================== // 
+	const blackToWhiteWidthRatio = realBlackWidth / realWhiteWidth;
+	const blackKeyWidth = blackToWhiteWidthRatio * size;
+	getLeft()
+
+	function getLeft() { 
+		const oldVer = (i * blackWidth) * size + 'px';
+		const newVer = (i * blackKeyWidth) + 'px';
+
+		console.log('oldVer', oldVer)
+		console.log('newVer', newVer)
+	}
+
 	// =========================== Helper Fns ===================== //
 	// ================ Color
 	function getKeyColor(key) {
 		return (key.indexOf('b') == - 1 ) ? 'white' : 'black';
 	}
-
-	function getLeft(i, whiteWidth, blackToWhiteWidthRatio) { 
-		return (i * blackToWhiteWidthRatio) * whiteWidth + 'px'; 
-	}
-
+ 
 	// =========================== Output ===================== //
 	return (  
 		<div className={`key ${keyColor}-key`} id={id} style={style}
